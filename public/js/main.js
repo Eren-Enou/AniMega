2452cc3442ee705c651f38053ef3316d0bcfa3dd
Eren-Enou <erenenou@yahoo.com>
2023-07-12 14:36:48 -0700

    description main.

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,26 @@
-//Practicing search code
+//UNUSED Practicing search code
+/*
+
+This code demonstrates a practice search implementation using GraphQL and Axios for making an API request. It includes code to define the search query, make an API request, handle the response, and export the search function.
+
+Here's an overview of what this code does:
+
+Declare a search query variable using GraphQL syntax to search for anime titles.
+Set the URL for the API endpoint.
+Set the options object for the API request, including the request method, headers, and data.
+Set up an event listener for the "DOMContentLoaded" event to execute code when the HTML document has finished loading.
+Get the search form element from the DOM.
+Attach an event listener to the form submit event to handle the search functionality.
+Get the search input element and retrieve the search term value when the form is submitted.
+Call the makeAPIRequest() function, passing in the search term.
+Define the makeAPIRequest() function to handle the API request logic.
+Log the search term for testing purposes.
+Define the searchData() function to make the HTTP API request using Axios, passing in the URL and options.
+Define the handleResponse() function to handle the API response and return the response data or throw an error.
+Export the searchData() function.
+This code demonstrates a basic search implementation using GraphQL and Axios, allowing you to make an API request and handle the response.
+
+*/
 
 var searchQuery = `
 query SearchAnimeName($search: String) {
