adb24df7b6326394bcf63c8373c982d6f2bd7c4d
Eren-Enou <erenenou@yahoo.com>
2023-07-12 14:35:10 -0700

    description animeCarousel.

--- public/js/animeCarousel.js
+++ public/js/animeCarousel.js
@@ -1,3 +1,30 @@
+/*
+
+This code sets up an event listener for the "DOMContentLoaded" event, which triggers the execution of JavaScript code related to a carousel functionality. It includes functions to update the carousel slide, navigate to the next slide, navigate to the previous slide, and handle window resize events.
+
+Here's an overview of what this code does:
+
+Set up event listeners for the "DOMContentLoaded" event, which ensures that the JavaScript code executes when the HTML document has finished loading.
+Retrieve references to the carousel element, previous button, and next button from the DOM.
+Initialize variables to keep track of the current slide and the total number of slides.
+Define a function named updateSlide() to update the carousel slide based on the window width and the current slide index.
+Calculate the item width based on the window width and a maximum value.
+Determine the number of visible slides based on the window width.
+Calculate the total width of the carousel based on the item width and the total number of slides.
+Calculate the offset for the current slide.
+Update the width and transform properties of the carousel element to display the appropriate slide.
+Define a function named nextSlide() to navigate to the next slide.
+Update the current slide index by incrementing it and using the modulo operator to ensure it stays within the range of the total number of slides.
+Call the updateSlide() function to update the display.
+Define a function named prevSlide() to navigate to the previous slide.
+Decrement the current slide index and wrap around to the last slide if it becomes less than 0.
+Call the updateSlide() function to update the display.
+Attach event listeners to the previous and next buttons to trigger the respective slide navigation functions on click events.
+Attach a window resize event listener to update the slide display when the window size changes.
+Call the updateSlide() function initially to set up the initial slide display.
+This code aims to provide a carousel functionality that can be used to navigate through a set of slides, with responsive behavior based on the window size.
+
+*/
 document.addEventListener("DOMContentLoaded", function() {
   var carousel = document.querySelector(".anime-carousel-list");
   var prevBtn = document.querySelector(".carousel-btn-prev");
