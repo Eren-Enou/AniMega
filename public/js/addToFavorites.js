23df437845317e8ed8bdc1cbe9a36c84b1a1a6b1
Eren-Enou <erenenou@yahoo.com>
2023-07-12 14:34:04 -0700

    description addToFavorites

--- public/js/addToFavorites.js
+++ public/js/addToFavorites.js
@@ -1,3 +1,24 @@
+/*
+
+This code defines a JavaScript function to handle a button click event for adding an anime to a user's favorites. It also includes helper functions to retrieve the current user ID and anime ID, as well as an event listener to attach the click event to the button.
+
+Here's an overview of what this code does:
+
+Define a function named addToFavorites() to handle the button click event.
+Retrieve the current user ID from your session using the getUserID() helper function.
+Retrieve the current anime page ID from your session using the getAnimeID() helper function.
+Create a data object containing the user ID and anime ID.
+Make an API request to the backend server using the fetch() function, sending the data as a JSON payload.
+Handle the response from the server:
+If the response status is OK, log a success message and perform any desired actions after the anime is added to favorites.
+If the response status is not OK, log an error message and handle the error case appropriately.
+Handle potential errors during the API request and provide appropriate error messages.
+Define a getUserID() helper function to retrieve the user ID dynamically from your authentication or session system.
+Define a getAnimeID() helper function to extract the anime ID from the current URL.
+Attach an event listener to the button with the ID 'addFavoriteButton' to trigger the addToFavorites() function on a click event.
+This code aims to enable adding an anime to a user's favorites by making an API request to the backend server based on the current user and anime information.
+
+*/
 
 // Function to handle the button click event
 function addToFavorites() {
